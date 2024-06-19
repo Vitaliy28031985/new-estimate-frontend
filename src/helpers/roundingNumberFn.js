0583b1a694514cde4c576539f6dbd9a254fd4f2e
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-06-19 15:15:56 +0300

    roundingNumberFn correct

--- src/helpers/roundingNumberFn.js
+++ src/helpers/roundingNumberFn.js
@@ -1,5 +1,21 @@
 function roundingNumberFn (number) {
-    return  number.toFixed(2);
+  let newNumber = null;
+  const convertNumber =  number.toFixed(2);
+  const numberArray = convertNumber.toString().split('');
+
+  if(numberArray[numberArray.length - 1] === "0" && numberArray[numberArray.length - 2] === "0") {
+    const newArray = numberArray.slice(0, numberArray.length - 3)
+    newNumber = newArray.join('');
+    return Number(newNumber);
+    }
+
+
+  if(numberArray[numberArray.length - 1] === "0") {
+  const newArray = numberArray.slice(0, numberArray.length - 1)
+  newNumber = newArray.join('');
+  return Number(newNumber);
+  }
+
   }
   
   export default roundingNumberFn;
