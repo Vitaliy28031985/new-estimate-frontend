ac71d7e8b0a18ebd192f107d783437670896d27e
Vitaliy <94307584+Vitaliy28031985@users.noreply.github.com>
2024-06-21 09:41:48 +0300

    connect admin function

--- src/components/Profile/AdminFunctions/AdminFunctions.jsx
+++ src/components/Profile/AdminFunctions/AdminFunctions.jsx
@@ -1,17 +1,57 @@
+import { useState} from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import {useCurrentQuery, useAddAllowUserMutation} from "../../../redux/auth/authApi";
 import s from "./AdminFunctions.module.scss";
 
 function AdminFunctions() {
+    const [email, setEmail] = useState('');
+    const [time, setTime] = useState('');
+
+
+    const {data} = useCurrentQuery();
+    const [addAllowUser] = useAddAllowUserMutation();
+
+    const handleChange = e => {
+        const {name, value} = e.currentTarget;
+        switch (name) {
+           case 'email':
+               setEmail(value);
+               break;
+           case 'time':
+               setTime(value);
+               break;
+           default:
+               return;  
+        }
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        if (email === '' || time === '') {
+            toast("Заповніть усі поля!");
+            return;
+        }
+        const allowObj = {
+            email,
+            time: Number(time)
+        }
+        await addAllowUser(allowObj);
+
+        setEmail('');
+        setTime('');
+    }
+    
     return (
         <div className={s.adminFunction}>
             <h3>Доступ користувачів</h3>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div className={s.input}>
-                <label id="data">Кількість днів</label>
-                <input type="number" name="data" />
+                <label for="time" id="time">Кількість днів</label>
+                <input type="number" name="time" id="time" onChange={handleChange} value={time} />
                 </div>
                 <div className={s.input}>
-                <label id="email">Email</label>
-                <input type="email" name="email" />
+                <label for="email" id="email">Email</label>
+                <input type="email" name="email" onChange={handleChange} value={email} />
                 </div>
                 <button type="submit">Надати</button>
             </form>
